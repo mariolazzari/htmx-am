c2d2b424d07955d7645801836d15a6334a04fbda
Mario Lazzari <mario.lazzari@gmail.com>
2026-07-17 08:28:20 +0200

    Done lesson 33

--- 2-places/views/components/location.js
+++ 2-places/views/components/location.js
@@ -1,7 +1,12 @@
 export default function renderLocation(location) {
   return `
     <li class="location-item">
-      <button hx-post="/places" hx-vals='{"locationId: "${location.id}"}'>
+      <button
+        hx-post="/places"
+        hx-vals='{"locationId: "${location.id}"}
+        hx-target="#interesting-locations"'
+        hx-swap="beforeend"
+      >
         <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
         <h3>${location.title}</h3>
       </button>
